2fe1a84c37d8d490f3bba65a3eadc7bd7d958c87
JishnuCode2 <88920598+JishnuCode2@users.noreply.github.com>
2023-10-28 10:46:32 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+
+const app = express()
+
+const server = require("http").Server(app)
+
+//app.get("/" , (req, res) => {
+    //res.status(200).send("Hello World")
+//})
+
+server.listen(3030)
+
+app.set("view engine", "ejs")
+
+app.use(express.static("public"))
+
+const {v4 : uuidv4} = require("uuid")
+
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get("/:room", (req, res)=>{
+    res.render("index", {roomId: req.params.room})
+})
